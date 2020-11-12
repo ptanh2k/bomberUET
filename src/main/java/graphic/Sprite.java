ff4b1e2cc97aa00a714b3ee862123be9f2658dc3
feelingBlue00 <pta230500x090800@gmail.com>
2020-11-12 14:11:19 +0700

    SpriteSheet completed

--- src/main/java/graphic/Sprite.java
+++ src/main/java/graphic/Sprite.java
@@ -1,4 +1,45 @@
 package graphic;
 
 public class Sprite {
+    public static final int DEFAULT_SIZE = 16;
+    public static final int SCALED_SIZE = DEFAULT_SIZE * 2;
+    public final int SIZE;
+    public int[] _pixels;
+
+    protected int _realWidth;
+    protected int _realHeight;
+
+    private int _x, _y;
+    private SpriteSheet _sheet;
+
+    private static final int TRANSPARENT_COLOR = 0xffff00ff;
+
+    public Sprite(int size, int x, int y, SpriteSheet sheet, int rw, int rh) {
+        SIZE = size;
+        _sheet = sheet;
+        _realHeight = rh;
+        _realWidth = rw;
+        load();
+    }
+
+
+    public Sprite(int size, int color) {
+        SIZE = size;
+
+    }
+
+    private void setColor(int color) {
+
+    }
+
+    private void load() {
+
+    }
+
+    /*
+	|--------------------------------------------------------------------------
+	| Board sprites
+	|--------------------------------------------------------------------------
+	 */
+    public static Sprite grass = new Sprite(DEFAULT_SIZE, )
 }

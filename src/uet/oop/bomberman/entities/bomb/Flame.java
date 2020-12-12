fa4c9bdf70d6693fb721c6061c4eac282c32f8f2
feelingBlue00 <pta230500x090800@gmail.com>
2020-12-12 07:07:19 +0700

    Handle bomber's death by flame

--- src/uet/oop/bomberman/entities/bomb/Flame.java
+++ src/uet/oop/bomberman/entities/bomb/Flame.java
@@ -1,11 +1,15 @@
 package uet.oop.bomberman.entities.bomb;
 
 import javafx.scene.image.Image;
+import uet.oop.bomberman.BombermanGame;
 import uet.oop.bomberman.entities.Bomber;
 import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.entities.EntityArr;
+import uet.oop.bomberman.entities.Map;
 import uet.oop.bomberman.entities.blocks.Brick;
 import uet.oop.bomberman.entities.enemy.Enemy;
+import uet.oop.bomberman.graphic.Sprite;
+import uet.oop.bomberman.sound.Sound;
 
 public abstract class Flame extends Entity {
     private boolean isVisible = true;
@@ -57,6 +61,11 @@ public abstract class Flame extends Entity {
         for (Bomber b : EntityArr.bombers) {
             if (this.intersects(b)) {
                 b.setAlive(false);
+                Sound.play("endgame3");
+                BombermanGame.gameOver = true;
+                setImg(Sprite.movingSprite(Sprite.bomber_die, Sprite.bomber_die1, Sprite.bomber_die2
+                        , this.animate, Sprite.DEFAULT_SIZE).getFxImage());
+                Map.createMapByLevel(BombermanGame.level);
             }
         }
     }
